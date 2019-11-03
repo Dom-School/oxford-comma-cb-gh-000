584ae28d39936af1bb883a035ffc519358868bb3
Dom December <21.domantas.gruodis@azuolynogimnazija.lt>
2019-11-03 19:47:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   last = array.pop
   if array.size > 1
     return array.join(", ") << ", and #{last}"
+  elsif array.size == 1
+    return "#{array[0]} and #{last}"
   else
     return last
   end
